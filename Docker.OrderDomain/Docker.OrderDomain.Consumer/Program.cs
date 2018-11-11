3b476c7cdf3401257633ddf298c04bd7b26b392b
Lucas F Valle <payvalle@gmail.com>
2018-11-11 13:50:55 -0200

    Grpc mapping, Grpc Server and Consumer. All without implementation yet.

---
+++ Docker.OrderDomain/Docker.OrderDomain.Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Docker.OrderDomain.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Docker.OrderDomain/Docker.OrderDomain.Grpc.Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using static Docker.OrderDomain.Grpc.OrderService;
+
+namespace Docker.OrderDomain.Grpc
+{
+    internal class OrderImplementation : OrderServiceBase
+    {
+        public override Task<SendOrderReply> SendOrder(SendOrderRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        const int Port = 50051;
+
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+
+            Server server = new Server
+            {
+                Services = { OrderService.BindService(new OrderImplementation()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("OrderDomain server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
